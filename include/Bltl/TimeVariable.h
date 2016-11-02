64a53775d535e9fccc5e0a41beea2bee72baff7b
Zhou Jun <zhoujun@mac.local>
2016-11-02 22:13:25 +0800

    add TimeVariable

---
+++ include/Bltl/TimeVariable.h
@@ -0,0 +1,28 @@
+#ifndef _TIME_VARIABLE_H_
+#define _TIME_VARIABLE_H_
+
+#include <iostream>
+#include <unordered_set>
+#include <utility>
+
+#include "Parameter.h"
+
+class TimeVariable: Parameter
+{
+public:
+    TimeVariable();
+    TimeVariable(char* str);
+    int value;
+	std::pair<int, int> range;
+private:
+    std::string get_name();
+    static std::unordered_set<std::string> pool;
+};
+
+
+
+#endif
+
+// Local Variables:
+// mode: c++
+// End:
