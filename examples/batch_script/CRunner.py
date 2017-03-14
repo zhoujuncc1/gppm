970d1bbb1ea1a019382220a8da14b1f6fdb7b08d
Zhou Jun <dcszhju@nus.edu.sg>
2017-03-14 23:14:12 +0800

    add script

---
+++ examples/batch_script/CRunner.py
@@ -0,0 +1,65 @@
+'''
+Created on 5 Apr 2016
+
+@author: zhoujun
+'''
+
+from parser import Parser
+import sys
+
+import xml.etree.ElementTree as ET
+import subprocess as sp
+
+
+def run(variable, template, model, xml_filename):
+    inputfilename = "input_%d.xml" % variable
+    outputfilename = "output_%d.xml" % variable
+
+    parser = Parser(template.generate_bltl(variable), template.generate_constraint())
+
+    xmlData = ET.parse(xml_filename)
+    updateXml(parser, xmlData)
+    xml_string = ET.tostring(xmlData.getroot())
+    xml_string=xml_string.replace("&amp;", "&")
+    infile=open(inputfilename, "w")
+    infile.write(xml_string)
+    infile.close()
+    command = "../%s %s >> %s"% (model, inputfilename, outputfilename)
+    sp.call(command, shell=True)
+
+
+def updateXml(parser, xmlData):
+    prds = xmlData.find("prds")
+    for prd in parser.prds:
+        xml_prd = ET.SubElement(prds, "prd")
+        xml_prd.text= prd
+    bltls = xmlData.find("bltls")
+    for bltl in parser.bltls:
+        xml_prd = ET.SubElement(bltls, "bltl")
+        xml_prd.text=bltl
+    
+    constraints = xmlData.find("constraints")
+    for constraint in parser.constraints:
+        xml_prd = ET.SubElement(constraints, "constraint")
+        xml_prd.text=constraint
+def parseXML(filename):
+    tree = ET.parse(filename)
+    return tree
+
+def batch(model, variables, templates, xml_filename):
+    for i in range(0, len(variables)):
+        run(variables[i], templates[i], model, xml_filename)
+
+def main(batch_filename):
+    with open(batch_filename) as f:
+        content = f.readlines()
+    model = content[0]
+    xml_filename = content[1]
+    variables = eval(content[2])
+    templates = eval[content[3]]
+    batch(model, variables, templates, xml_filename)
+    
+if __name__=='__main__':
+    if(len(sys.argv)==1):
+        print "Input file with 4 lines: model=str, xml_filename=str, variables=[], templates=[]"
+    main(sys.argv[1])
---
+++ examples/batch_script/templates.py
@@ -0,0 +1,107 @@
+'''
+Created on 10 Aug 2016
+
+@author: zhoujun
+'''
+
+class Template(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, name, bltl_template, constraint_str):
+        '''
+        Constructor
+        '''
+        self.name = name
+        self.bltl_template = bltl_template
+        
+        self.constraint_str = constraint_str
+        self.weight = {}
+    def generate_bltl(self, *args):
+        result = self.bltl_template.format(*args)
+        return result
+    def generate_constraint(self):
+        return self.constraint_str
+
+class Templates(object):
+    templates = {}
+    templates['up'] = Template('up',
+                         '''Predicates:
+        
+                        p1:{0}::
+        
+                        p2:{0}::
+    
+                        Formulas:
+
+                        p1
+                        
+                        (F[k1]G[k2]p2)
+                        ''',
+                        None
+                        )
+    templates['updown_stay'] = Template('updown_stay',
+                         '''Predicates:
+    
+                        p1:{0}::
+    
+                        p2:{0}::
+    
+                        p3:{0}::
+    
+                        Formulas:
+    
+                        p1
+                        
+                        F[k1](p2 & F[k2]G[k3]p3)''',
+                         
+                         '''p3<p2'''
+        				)
+      
+    templates['updown'] = Template('updown',
+                         '''Predicates:
+    
+                        p1:{0}::
+    
+                        p2:{0}::
+    
+                        p3:{0}::
+    
+                        Formulas:
+    
+                        p1
+                        
+                        F[k1](p2 & F[k2]p3)''',
+                        '''p2>p3'''
+        				)
+    
+    templates['osci'] = Template('5',
+                          '''Predicates:
+     
+                         p1:{0}::
+     
+                         p2:{0}::
+     
+                         p3:{0}::
+     
+                         p4:{0}::
+                         
+                         Formulas:
+     
+                         p1 
+                         
+                         F[k1](p2 & F[k2](p3 & F[k3]p4))''',
+ 						'''p3<p2
+                         
+                         p3<p4'''
+                         
+         )
+    
+if __name__ == '__main__':
+    t = Templates.templates['6']
+    result = t.generate_bltl('x1')
+    print result
+    
+    
