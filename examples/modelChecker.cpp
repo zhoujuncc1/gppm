ad0eeae2fd1700e6b1be82d4449cafd3fa3ad04e
zhoujun <dcszhju@nus.edu.sg>
2019-02-28 20:06:09 +0800

    check

---
+++ examples/modelChecker.cpp
@@ -0,0 +1,42 @@
+/*
+ * modelChecker.cpp
+ *
+ *  Created on: Feb 27, 2019
+ *      Author: zhoujun
+ */
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include "ModelChecking.h"
+
+int main(int argc, char** argv){
+    using namespace std;
+    if(argc<2){
+        printf("ERROR: must give input file name!\n");
+        exit(1);
+    }
+    clock_t begin = clock();
+    string filename(argv[1]);
+    std::ifstream f(filename);
+    if (f.is_open())
+        std::cout << f.rdbuf();
+    f.close();
+    vector<ModelChecker*> checkers = ModelChecker::buildChecker(filename);
+    vector<int> n_pass; 
+    for(auto itr = checkers.begin(); itr!=checkers.end(); itr++){
+        vector<int> results = (*itr)->check();
+        int sum = 0;
+        for(int i= 0; i < results.size(); i++)
+            sum+=results[i];
+        n_pass.push_back(sum);
+    }
+    for(int i = 0; i < n_pass.size(); i++)
+        cout<<n_pass[i]<<endl;
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout << "Time: " << elapsed_secs << endl;
+    return 0;
+
+}
+
--- include/ModelChecking.h
+++ include/ModelChecking.h
@@ -5,6 +5,16 @@
 #ifndef GPPM_MODELCHECKING_H
 #define GPPM_MODELCHECKING_H
 #include "Model.h"
+#include <map>
+#include <vector>
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+#include "Bltl/Bltl.h"
+#include "Bltl/bltl_parser.h"
+#include "Bltl/BltlChecker.h"
 
 #define N_SIM 100
 #define MAX_SIM 1000
@@ -12,8 +22,61 @@
 #define BAYES_MAX 100
 #define LOSS_MAX 1000000
 
-class State;
+using namespace std;
+namespace pt = boost::property_tree;
 
+
+class State;
 double modelchecking(State *state);
 double loss(State* state);
+
+
+class ModelChecker{
+public:
+    map<string, Prd*> prds;
+    Bltl* bltl;
+    vector<Trajectory> trajectories;
+    BltlChecker* bltlChecker;
+    ModelChecker(string bltl_input, vector<string> prd_inputs, string filename){
+        prds = parse_prd(prd_inputs);
+        bltl = parse_bltl(bltl_input);
+        link_prd(bltl, prds);
+        FileTrajectoryProvider trajProvider(filename);
+        trajectories = trajProvider.getTrajectories(MAX_SIM);
+        bltlChecker= new RecursiveBltlChecker(bltl, prds, trajectories[0]);
+
+    }
+
+    vector<int> check(){
+        vector<int> results;
+        for(int i = 0; i < trajectories.size(); i++)
+            results.push_back(bltlChecker->check(trajectories[i]));
+        return results;
+    }
+
+	static vector<ModelChecker*> buildChecker(string filename){
+        vector<ModelChecker*> checkers;
+
+        pt::ptree input;
+        pt::read_xml(filename, input, pt::xml_parser::trim_whitespace);
+
+        vector<string> bltl_inputs;
+        BOOST_FOREACH(pt::ptree::value_type &v, input.get_child("input.bltls")) {
+            // The data function is used to access the data stored in a node.
+            bltl_inputs.push_back(v.second.data());
+        }
+        vector<string> prd_inputs;
+        BOOST_FOREACH(pt::ptree::value_type &v, input.get_child("input.prds")) {
+            // The data function is used to access the data stored in a node.
+            prd_inputs.push_back(v.second.data());
+        }
+        string traj_filename = input.get<string>("input.filename");
+
+        for(auto itr = bltl_inputs.begin(); itr != bltl_inputs.end(); itr++)
+            checkers.push_back(new ModelChecker(*itr+";", prd_inputs, traj_filename));
+        return checkers;
+	}
+
+};
+
 #endif //GPPM_MODELCHECKING_H
